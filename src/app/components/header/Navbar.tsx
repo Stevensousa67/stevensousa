b028723169e05da3f57270d6d03184b6d1fca8ae
Steven Sousa <silver.steven@hotmail.com>
2025-05-18 01:33:12 -0400

    added shadcn components, added dark/light mode

--- src/app/components/header/Navbar.tsx
+++ src/app/components/header/Navbar.tsx
@@ -1,9 +1,11 @@
 'use client';
 import React, { useState } from 'react';
-import Image from 'next/image';
 import Link from 'next/link';
 import NavbarLinks from './NavbarLinks';
 import classes from './navbar.module.css';
+import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar';
+import { ModeToggle } from '@/components/ui/mode-toggle';
+import { Toggle } from '@/components/ui/toggle';
 
 export default function Navbar() {
     const [isOpen, setIsOpen] = useState(false);
@@ -19,13 +21,19 @@ export default function Navbar() {
 
     return (
         <header>
-            <nav className="sticky w-full z-20 top-0 left-0 mb-16" role="navigation">
+            <nav className="fixed w-full z-20 top-0 left-0 mb-16" role="navigation">
                 <div className="flex justify-center px-4 md:px-0">
                     <div className="inline-flex items-center px-6 py-3 bg-white/20 border border-white/30 rounded-full shadow-xl mt-5 backdrop-blur-lg transition-all w-full md:w-auto md:max-w-2xl lg:max-w-4xl">
-                        <Link href='/' className="inline-block mr-2 md:mr-8">
-                            <Image src='/Logo.jpeg' alt='Steven Sousa Logo Image' width={60} height={60} />
+                        <Link href='/' className="inline-block mr-2 md:mr-6">
+                            <Avatar>
+                                <AvatarImage src='/Logo.jpeg'/>
+                                <AvatarFallback>Logo</AvatarFallback>
+                            </Avatar>
                         </Link>
                         <NavbarLinks className="hidden md:flex gap-8 text-lg" />
+                        <div className="border-l h-6 mx-4 border-black dark:border-white" aria-hidden="true"></div>
+
+                        <ModeToggle />
                         <button className="md:hidden text-black ml-auto" onClick={() => (isOpen ? closeMenu() : setIsOpen(true))} aria-label={isOpen ? 'Close menu' : 'Open menu'} aria-expanded={isOpen}>
                             {isOpen ? (
                                 <svg className="w-8 h-8" fill="none" stroke="currentColor" strokeWidth={2.5} viewBox="0 0 24 24">
