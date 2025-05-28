f395928e184a3a177dbfbc3acd84e8770f153aa3
Steven Sousa <silver.steven@hotmail.com>
2025-05-28 15:37:01 -0400

    added 3 real projects to portfolio.

--- src/app/components/body/sections/projects/Projects.tsx
+++ src/app/components/body/sections/projects/Projects.tsx
@@ -1,15 +1,13 @@
-import React from 'react'
-import Image from 'next/image'
-import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "@/components/ui/card"
+import React from 'react';
+import Image from 'next/image';
+import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "@/components/ui/card";
 import { Button } from "@/components/ui/button";
 
 export default function Projects() {
   return (
-    <section className="py-16 flex flex-col items-center text-center gap-8">
+    <section className="py-10 flex flex-col items-center text-center gap-8">
       <h1 className="text-4xl font-bold">Featured Projects</h1>
-      <p className="text-2xl text-primary">Here are some of my projects</p>
       <div className="flex flex-wrap gap-8 justify-center">
-
         {/* project cards 1 */}
         <Card className='w-80'>
           <CardHeader>
@@ -17,10 +15,10 @@ export default function Projects() {
             <CardDescription>Under Development</CardDescription>
           </CardHeader>
           <CardContent className="flex justify-center items-center">
-            <Image src="/Holy Bible.jpg" alt="Holy Bible" className="rounded-lg" width={420} height={420}></Image>
+            <Image src="/Holy Bible.jpg" alt="Holy Bible" className="rounded-lg object-cover" width={420} height={420}></Image>
           </CardContent>
-          <CardFooter>
-            <CardDescription>A full-stack web application of the Holy Bible built using Next.JS, Tailwind CSS, PostgreSQL, and APIs. </CardDescription>
+          <CardFooter className="flex flex-col items-center">
+            <CardDescription>Grow yourself spiritually and get closer to God by reading the Scriptures, studying customized study plans and devotionals.</CardDescription>
           </CardFooter>
         </Card>
 
@@ -28,12 +26,13 @@ export default function Projects() {
         <Card className='w-80'>
           <CardHeader>
             <CardTitle>Resume Builder</CardTitle>
+            <CardDescription className="opacity-0">Live</CardDescription>
           </CardHeader>
           <CardContent className="flex justify-center items-center">
-            <Image src="/Resume Builder.jpg" alt="Resume Builder" className="rounded-lg" width={420} height={420}></Image>
+            <Image src="/Resume Builder.jpg" alt="Resume Builder" className="rounded-lg object-cover" width={420} height={420}></Image>
           </CardContent>
           <CardFooter className='flex flex-col items-center'>
-            <CardDescription>A full-stack web application that leverages Google Gemini to build resumes and cover letters tailored specifically to job descriptions. Built using Django, Tailwind CSS, and PostgreSQL, and deployed on AWS EC2. </CardDescription>
+            <CardDescription>Leverage Google Gemini AI to build resumes and cover letters tailored specifically to a selected job description.</CardDescription>
             <Button asChild className="mt-4"><a href="https://resumebuilder.stevensousa.com" target="_blank" rel="noopener noreferrer">View Project</a></Button>
           </CardFooter>
         </Card>
@@ -41,13 +40,14 @@ export default function Projects() {
         {/* project cards 3 */}
         <Card className='w-80'>
           <CardHeader>
-            <CardTitle>Soccer</CardTitle>
+            <CardTitle>Football</CardTitle>
+            <CardDescription className="opacity-0">Live</CardDescription>
           </CardHeader>
           <CardContent className="flex justify-center items-center">
-            <Image src="/Soccer.jpg" alt="Soccer" className="rounded-lg" width={420} height={420}></Image>
+            <Image src="/Soccer.jpg" alt="Soccer" className="rounded-lg object-cover" width={420} height={420}></Image>
           </CardContent>
           <CardFooter className="flex flex-col items-center">
-            <CardDescription>A full-stack web application for soccer fans to keep track of major league standings, game schedules, and news. Built using Express, Bootstrap, PostgreSQL, ESPN APIs, and GCP. </CardDescription>
+            <CardDescription>Track major football league standings, game schedules, and news. Perfect for those football junkies needing a quick fix.</CardDescription>
             <Button asChild className="mt-4"><a href="https://soccer.stevensousa.com" target="_blank" rel="noopener noreferrer">View Project</a></Button>
           </CardFooter>
         </Card>
@@ -55,5 +55,5 @@ export default function Projects() {
       <p className="text-lg text-gray-500">More projects coming soon!</p>
       <a href="/projects" className="text-lg text-blue-500 hover:underline">View all projects</a>
     </section>
-  )
+  );
 }
