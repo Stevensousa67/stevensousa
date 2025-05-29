a0c024922cb21099399c2b82140589143e13560c
Steven Sousa <silver.steven@hotmail.com>
2025-05-29 10:32:19 -0400

    updated navbar font

--- src/app/components/header/NavbarLinks.tsx
+++ src/app/components/header/NavbarLinks.tsx
@@ -4,9 +4,9 @@ export default function NavbarLinks({className}: {className?: string}) {
     return (
         <ul className={className}>
             {navbarItems.map((item) => (
-                <li key={item} className="relative group cursor-pointer">
+                <li key={item} className="relative group cursor-pointer text-sm font-medium text-muted-foreground hover:text-foreground">
                     {item}
-                    <span className="absolute left-0 bottom-[-5px] w-0 h-1 rounded-xl bg-red-400 transition-all duration-300 group-hover:w-full"></span>
+                    {/* <span className="absolute left-0 bottom-[-5px] w-0 h-1 rounded-xl bg-red-400 transition-all duration-300 group-hover:w-full"></span> */}
                 </li>
             ))}
         </ul>
