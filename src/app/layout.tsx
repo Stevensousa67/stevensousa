0f5a9e2178dbdea5b1b905bb0d279af82fa7d420
Steven Sousa <silver.steven@hotmail.com>
2025-06-04 21:41:18 -0400

    fixed webpage shift when drawer is open

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -27,7 +27,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" suppressHydrationWarning>
-      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+      <body className={`${geistSans.variable} ${geistMono.variable} antialiased overflow-y-scroll`}>
         <ThemeProvider attribute="class" defaultTheme="system">{children}</ThemeProvider>
         <SpeedInsights />
         <Analytics />
