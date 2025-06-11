dbaa219072220e5ed5209de2ddba89e7d9589aa3
Steven Sousa <silver.steven@hotmail.com>
2025-06-11 15:26:20 -0400

    commenting out Link import

--- src/app/projects/ProjectCard.tsx
+++ src/app/projects/ProjectCard.tsx
@@ -1,5 +1,5 @@
 import Image from 'next/image';
-import Link from 'next/link';
+// import Link from 'next/link';
 import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "@/components/ui/card";
 import { Button } from "@/components/ui/button";
 import { AspectRatio } from "@/components/ui/aspect-ratio";
