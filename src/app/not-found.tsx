443fc3105c069bb4e9fa5b00bf7d60eb6cf4061b
Steven Sousa <silver.steven@hotmail.com>
2025-06-13 08:00:27 -0400

    made text muted

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -12,7 +12,7 @@ export default function NotFound() {
     const baseURL = process.env.NEXT_PUBLIC_AWS_S3_BASE_URL;
 
     return (
-        <div className="min-h-screen flex flex-col bg-gradient-to-b from-black to-gray-900 text-white">
+        <div className="min-h-screen flex flex-col">
             <Navbar />
 
             <main className="flex-grow w-full max-w-5xl mx-auto">
@@ -39,14 +39,14 @@ export default function NotFound() {
                         <Image src={`${baseURL}Astronaut.jpg`} alt="Astronaut floating in space" className="object-cover rounded-lg" width={1920} height={1080} />
                     </motion.div>
                 </AspectRatio>
-                <motion.p className="text-xl md:text-2xl text-center mb-8 -mt-42" initial={{ opacity: 0 }} animate={{ opacity: 1 }} transition={{ delay: 0.4, duration: 0.8 }}>
+                <motion.p className="text-lg text-muted-foreground text-center mb-8 -mt-42" initial={{ opacity: 0 }} animate={{ opacity: 1 }} transition={{ delay: 0.4, duration: 0.8 }}>
                     You&apos;ve drifted into deep space! Let&apos;s get you back to Earth.
                 </motion.p>
                 <div className="text-center">
                     <Button asChild className="hover:underline">
                         <Link href="/">Return to Earth</Link>
                     </Button>
-                </div>
+                </div>  
             </main>
             <div className="max-w-5xl mx-auto w-full">
                 <Footer />
