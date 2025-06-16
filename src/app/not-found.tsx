e499a4bcc2dd00c92358f6e266e6da3c649269cd
Steven Sousa <silver.steven@hotmail.com>
2025-06-16 08:49:50 -0400

    improved margins between moving picture

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -11,10 +11,10 @@ export default function NotFound() {
     return (
         <>
             <AppBreadcrumb currentPage="Lost in Space" />
-            <motion.p className="text-3xl font-semibold mt-20 mb-8 text-center" initial={{ opacity: 0, y: -20 }} animate={{ opacity: 1, y: 0 }} transition={{ duration: 0.8 }}>
+            <motion.p className="text-3xl font-semibold mt-20 mb-4 text-center" initial={{ opacity: 0, y: -20 }} animate={{ opacity: 1, y: 0 }} transition={{ duration: 0.8 }}>
                 404 - Houston, We Have a Problem!
             </motion.p>
-            <div className="w-70 mx-auto aspect-[16/9] mb-8">
+            <div className="w-70 mx-auto aspect-[16/9] mb-4">
                 <motion.div animate={{ y: [-5, 5] }} transition={{ repeat: Infinity, repeatType: "reverse", duration: 3, ease: "easeInOut" }}>
                     <Image src={`${baseURL}Astronaut.jpg`} alt="Astronaut floating in space" className="object-cover rounded-lg w-full h-full" width={1920} height={1080} sizes="(max-width: 640px) 100vw, (max-width: 768px) 80vw, 50vw" priority />
                 </motion.div>
