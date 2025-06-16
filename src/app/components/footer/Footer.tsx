1fefca3a259c47ea05ca7c154a1d0b673c78073e
Steven Sousa <silver.steven@hotmail.com>
2025-06-16 08:50:55 -0400

    re added Spotify component to Footer

--- src/app/components/footer/Footer.tsx
+++ src/app/components/footer/Footer.tsx
@@ -10,7 +10,7 @@ export default function Footer() {
       <div className="relative z-10 flex items-center md:gap-4">
         <Image src={`${baseUrl}Spotify+Logo+Icon.svg`} alt="Spotify Logo" width={20} height={20} className="h-6 w-6" />
         <div className="inline-flex w-full items-center justify-center gap-1 text-sm md:justify-start">
-          {/* <SpotifyStatus /> */}
+          <SpotifyStatus />
         </div>
       </div>
       <div className="absolute inset-0 bg-gradient-radial from-blue-300/20 via-transparent to-transparent blur-3xl opacity-70 pointer-events-none" />
