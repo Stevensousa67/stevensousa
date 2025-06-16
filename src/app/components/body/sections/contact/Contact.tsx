a343a2be97f72b11e4971552915370f4540b6086
Steven Sousa <silver.steven@hotmail.com>
2025-06-16 13:11:02 -0400

    added margin bottom and updated verbiage

--- src/app/components/body/sections/contact/Contact.tsx
+++ src/app/components/body/sections/contact/Contact.tsx
@@ -4,10 +4,10 @@ import { ContactForm } from './ContactForm';
 export default function Contact() {
   return (
     <>
-      <section className="mt-30 mb-20 flex flex-col text-center gap-8">
+      <section className="mt-30 mb-10 p-6 flex flex-col text-center gap-8">
         <h1 className="text-3xl font-semibold">Contact</h1>
         <ContactForm />
-        <p className="text-lg text-muted-foreground">Feel free to reach out for collaborations or just a chat!</p>
+        <p className="text-lg text-muted-foreground">Reach out! Let&apos;s collaborate or just chat!</p>
       </section>
     </>
   );
