bab9b8ee1ebcfa287aa100ec44c6c026271b2bce
Steven Sousa <silver.steven@hotmail.com>
2025-06-17 14:54:21 -0400

    added hover effect over skill badges

--- src/app/components/body/sections/skills/Skills.tsx
+++ src/app/components/body/sections/skills/Skills.tsx
@@ -12,7 +12,7 @@ export default function Skills() {
         <h1 className="text-3xl font-semibold">Skills</h1>
         <div className="flex flex-wrap gap-8 justify-center">
           {skills.map((skill) => (
-            <Badge key={skill.name} variant="outline" className="flex items-center gap-4 px-2 py-2">
+            <Badge key={skill.name} variant="outline" className="flex items-center gap-4 px-2 py-2 transform transition duration-300 ease-in-out hover:-translate-y-1 hover:shadow-lg hover:border-blue-500">
               <Image src={skill.icon} alt={skill.name} width={18} height={18} className={skill.isBlack ? "invert-0 dark:invert" : ""} />
               <span className="sr-only">{skill.name} icon</span>
               <span className="hidden sm:inline">{skill.name}</span>
