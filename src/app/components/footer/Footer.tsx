3314e509390c934b042c9aa9bbc18b743f305900
Steven Sousa <silver.steven@hotmail.com>
2025-06-17 15:43:04 -0400

    Improved background gradient of footer

--- src/app/components/footer/Footer.tsx
+++ src/app/components/footer/Footer.tsx
@@ -6,7 +6,8 @@ export default function Footer() {
   const baseUrl = process.env.NEXT_PUBLIC_AWS_S3_BASE_URL;
 
   return (
-    <footer className="bg-background/30 shadow-xs relative max-w-5xl mx-auto w-full mb-3 flex flex-col rounded-2xl p-8 saturate-100 backdrop-blur-[10px] overflow-hidden">
+    <footer className="relative max-w-5xl mx-auto w-full mb-3 flex flex-col rounded-2xl p-8 saturate-100 backdrop-blur-[10px] overflow-hidden
+  bg-gradient-to-t from-white/30 via-white/10 to-white/0 dark:from-black/30 dark:via-black/10 dark:to-black/0 shadow-xs">
       <div className="relative z-10 flex items-center md:gap-4">
         <Image src={`${baseUrl}Spotify+Logo+Icon.svg`} alt="Spotify Logo" width={20} height={20} className="h-6 w-6" />
         <div className="inline-flex w-full items-center justify-center gap-1 text-sm md:justify-start">
