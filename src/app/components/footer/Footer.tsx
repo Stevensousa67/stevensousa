d8c8d5427d2c35589600ba96692e202adce92289
Steven Sousa <silver.steven@hotmail.com>
2025-06-18 09:38:02 -0400

    updated baseUrl for footer svgs

--- src/app/components/footer/Footer.tsx
+++ src/app/components/footer/Footer.tsx
@@ -3,13 +3,14 @@ import Image from "next/image";
 import SpotifyStatus from './SpotifyStatus';
 
 export default function Footer() {
-  const baseUrl = process.env.NEXT_PUBLIC_AWS_S3_BASE_URL;
+  const baseUrl = `${process.env.NEXT_PUBLIC_AWS_S3_BASE_URL}svgs/`;
+  
 
   return (
     <footer className="relative max-w-5xl mx-auto w-full mb-3 flex flex-col rounded-2xl p-8 saturate-100 backdrop-blur-[10px] overflow-hidden
   bg-gradient-to-t from-white/30 via-white/10 to-white/0 dark:from-black/30 dark:via-black/10 dark:to-black/0 shadow-xs">
       <div className="relative z-10 flex items-center md:gap-4">
-        <Image src={`${baseUrl}Spotify+Logo+Icon.svg`} alt="Spotify Logo" width={20} height={20} className="h-6 w-6" />
+        <Image src={`${baseUrl}Spotify.svg`} alt="Spotify Logo" width={20} height={20} className="h-6 w-6" />
         <div className="inline-flex w-full items-center justify-center gap-1 text-sm md:justify-start">
           <SpotifyStatus />
         </div>
