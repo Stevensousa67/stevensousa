781d0763cfe82e7d78432b6aaac311df1a49247b
Steven Sousa <silver.steven@hotmail.com>
2025-06-18 08:21:28 -0400

    updated component to use Shadcn Avatar & updated baseUrl

--- src/app/components/body/sections/about/Picture&Skeleton.tsx
+++ src/app/components/body/sections/about/Picture&Skeleton.tsx
@@ -1,17 +1,24 @@
 'use client';
 import React, { useState } from 'react';
-import Image from 'next/image';
 import { Skeleton } from '@/components/ui/skeleton';
+import { Avatar, AvatarImage } from '@/components/ui/avatar';
 
 export default function PictureAndSkeleton() {
-    const baseUrl = process.env.NEXT_PUBLIC_AWS_S3_BASE_URL;
+    const baseUrl = `${process.env.NEXT_PUBLIC_AWS_S3_BASE_URL}ss-pictures/`;
     const imageSrc = `${baseUrl}Steven+Sousa.jpeg`;
     const [isImageLoaded, setIsImageLoaded] = useState(false);
 
     return (
-        <div className="relative overflow-hidden rounded-full w-40 h-40">
-            {!isImageLoaded && <Skeleton className="w-40 h-40 rounded-full absolute inset-0 z-0" />}
-            <Image src={imageSrc} alt="Steven Sousa" width={160} height={160} className={`rounded-full object-cover relative z-10 transition-opacity duration-500 ease-in-out ${!isImageLoaded ? 'opacity-0' : 'opacity-100'}`} onLoad={() => setIsImageLoaded(true)}/>
+        <div className="relative w-40 h-40 rounded-full">
+            {(!isImageLoaded) && (<Skeleton className="w-40 h-40 rounded-full absolute inset-0 z-0" />)}
+            <Avatar className="relative z-10 w-40 h-40 rounded-full overflow-hidden">
+                <AvatarImage
+                    src={imageSrc}
+                    alt="Steven Sousa"
+                    onLoad={() => setIsImageLoaded(true)}
+                    className={`object-cover w-full h-full object-[center_top] transition-opacity duration-500 ease-in-out ${!isImageLoaded ? 'opacity-0' : 'opacity-100'}`}
+                />
+            </Avatar>
         </div>
     );
 }
