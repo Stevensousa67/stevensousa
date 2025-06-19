72b98ffa08d46d2ce03e6fcec6abeb793f6f13da
Steven Sousa <silver.steven@hotmail.com>
2025-06-19 14:54:02 -0400

    refactored about me paragraphs into its own file

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,5 +1,6 @@
 "use client";
 import Image from 'next/image';
+import { aboutMe } from '@/lib/aboutMe';
 import { motion } from 'framer-motion';
 
 export default function AboutPage() {
@@ -18,22 +19,15 @@ export default function AboutPage() {
 
     return (
         <>
-            <motion.p className="text-3xl font-semibold mt-10 mb-10 text-center" initial={{ opacity: 0, y: -20 }} animate={{ opacity: 1, y: 0 }} transition={{ duration: 0.5 }}>
-                About Me
-            </motion.p>
+            <p className="text-3xl font-semibold mt-10 mb-10 text-center animate-fade-slide-down">About Me</p>
             <div className="flex flex-col md:flex-row items-center justify-center gap-2 mb-10 px-4">
-                {/* Changed flex-1 to md:flex-1 */}
                 <motion.div className="flex-1 mb-10" initial={{ opacity: 0, scale: 0.8 }} animate={{ opacity: 1, scale: 1 }} transition={{ duration: 0.6, ease: 'easeOut' }}>
                     <Image src={imageSrc} alt="Steven Sousa" width={250} height={250} className="rounded-2xl object-cover mx-auto" />
                 </motion.div>
                 <motion.div className="flex-1 text-center md:text-left" variants={container} initial="hidden" animate="visible">
-                    {[
-                        "Hi there! I'm Steven Sousa, a Full Stack Engineer on a mission to make the Internet a better place, one project at a time.",
-                        "I specialize in crafting seamless web experiences with tools like React, Node.js, and TypeScript. My passion lies in solving complex problems and building scalable, user-focused solutions that leave a lasting impact.",
-                        "When I'm not coding, you’ll find me strumming my guitar, hitting the gym, gaming, or rolling on the mats practicing Brazilian Jiu-Jitsu (Oss!). Personal projects keep my creativity flowing, too.",
-                    ].map((text, index) => (
+                    {aboutMe.map((paragraph, index) => (
                         <motion.p key={index} className="text-lg mb-4 last:mb-0" variants={item}>
-                            {text}
+                            {paragraph.paragraph}
                         </motion.p>
                     ))}
                 </motion.div>
---
+++ src/lib/aboutMe.ts
@@ -0,0 +1,18 @@
+interface AboutMe {
+  paragraph: string;
+}
+
+export const aboutMe: AboutMe[] = [
+  {
+    paragraph:
+      "Hi there! I'm Steven Sousa, a Full Stack Engineer on a mission to make the Internet a better place, one project at a time.",
+  },
+  {
+    paragraph:
+      "I specialize in crafting seamless web experiences with tools like React, Node.js, and TypeScript. My passion lies in solving complex problems and building scalable, user-focused solutions that leave a lasting impact.",
+  },
+  {
+    paragraph:
+      "When I'm not coding, you’ll find me strumming my guitar, hitting the gym, gaming, or rolling on the mats practicing Brazilian Jiu-Jitsu (Oss!). Personal projects keep my creativity flowing, too.",
+  },
+];
